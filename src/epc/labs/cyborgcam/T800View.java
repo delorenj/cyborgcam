6fd18ee714528d45ac2d598d8458f134f10bf7c9
Jarad DeLorenzo <jaradd@gmail.com>
2010-11-23 11:55:27 -0500

    Eclipse sync

--- src/epc/labs/cyborgcam/T800View.java
+++ src/epc/labs/cyborgcam/T800View.java
@@ -7,7 +7,6 @@ import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.util.Pair;
 import android.view.View;
 
 public class T800View extends View {
